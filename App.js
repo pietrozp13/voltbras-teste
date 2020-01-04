24a9afb45d5f456590194b38f35384a33aabf21d
Pietrozp13 <pietro.perboni@softplan.com.br>
2020-01-04 17:08:41 -0300

    testes with map

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const screen = Dimensions.get('window');
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView style={styles.mapStyle} 
+          initialRegion={{
+            latitude: -27.6229249,
+            longitude: -48.6796361,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: -27.6229249,
+              longitude: -48.6796361,
+            }}
+          />
+          <Marker
+            coordinate={{
+              latitude: -26.905948,
+              longitude: -48.66227,
+            }}
+          />
+        </MapView>
+        <View style={styles.itemContainer}>
+          <View
+              key={'123'}
+              style={styles.item}
+            />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -15,5 +46,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  mapStyle: {
+    width: screen.width,
+    height: screen.height,
+  },
+  itemContainer: {
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    position: 'absolute',
+    top: 504,
+  },
+  item: {
+    width: screen.width,
+    height: screen.height,
+    backgroundColor: 'gray',
+    marginHorizontal: 10,
+    opacity: 0.8,
+    overflow: 'hidden',
+    borderRadius: 20
   },
 });
