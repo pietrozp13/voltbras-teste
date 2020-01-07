7e44a32092a5bd585b9df51c1772c059b4e3b802
Pietrozp13 <pietro.perboni@softplan.com.br>
2020-01-06 21:56:12 -0300

    changes on animation triggers

--- src/InfoStation/InforStation.js
+++ src/InfoStation/InforStation.js
@@ -1,75 +1,37 @@
 import React from 'react'
-import { Animated, Dimensions } from 'react-native';
+import { Animated, TouchableWithoutFeedback } from 'react-native';
 
 import {ItemContainer, Item, ItemText } from './styles';
 
-// import { GestureHandler } from 'expo';
-// const { PanGestureHandler, State}  = GestureHandler;
-
-import { PanGestureHandler, State } from 'react-native-gesture-handler'
-
 
 export default function InforStation() {
-    let offset = 0;
+    let isOpen = false
+
     const translateY = new Animated.Value(0);
 
-    const animatedEvent = Animated.event(
-        [
-            {
-                nativeEvent: {
-                    translationY: translateY,
-                },
-            },
-        ],
-        { useNativeDriver: true },
-    );
+    function handleOnClickAnimation () {
 
-    function onHandlerStateChanged(event) {
-        if (event.nativeEvent.oldState === State.ACTIVE) {
-          let opened = false;
-          const { translationY } = event.nativeEvent;
-    
-          offset += translationY;
-    
-          if (translationY >= 100) {
-            opened = true;
-          } else {
-            translateY.setValue(offset);
-            translateY.setOffset(0);
-            offset = 0;
-          }
-    
-          Animated.timing(translateY, {
-            toValue: opened ? 380 : 0,
-            duration: 200,
-            useNativeDriver: true,
-          }).start(() => {
-            offset = opened ? 380 : 0;
-            translateY.setOffset(offset);
-            translateY.setValue(0);
-          });
-        }
-      }
+      Animated.timing(translateY, {
+        toValue: isOpen ? 0 : -490,
+        duration: 200,
+        useNativeDriver: true,
+      }).start();
+      isOpen = !isOpen
+    }
     
 
     return (
-        <PanGestureHandler
-            onGestureEvent={animatedEvent}
-            onHandlerStateChange={onHandlerStateChanged}
+      <TouchableWithoutFeedback onPress={() => handleOnClickAnimation()}>
+        <ItemContainer style={{
+            transform: [{
+                translateY: translateY,
+            }],
+          }}
         >
-            <ItemContainer style={{
-                transform: [{
-                    translateY: translateY.interpolate({
-                    inputRange: [-350, 0, 380],
-                    outputRange: [-50, 0, 380],
-                    extrapolate: 'clamp',
-                    }),
-                }],
-                }}>
-                <Item key={'123'}>
-                    <ItemText>testeeee</ItemText>
-                </Item>
-            </ItemContainer>
-        </PanGestureHandler>
+            <Item>
+                <ItemText>testeeee</ItemText>
+            </Item>
+        </ItemContainer>
+      </TouchableWithoutFeedback>
     )
 }
